9d1e067eaceadd5944121bf59c28b03e6271816c
tomoyuen <wisyue@yahoo.com>
2017-01-08 00:45:08 +0800

    remove useless package;

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,1 @@
-import Vue from 'vue';
-import VueResource from 'vue-resource';
-
-Vue.use(VueResource);
-
-// http相关
-Vue.http.options.crossOrigin = true;
-Vue.http.options.xhr = { withCredentials: true };
-
-/* eslint no-new: off */
-new Vue({
-  el: 'body',
-  data: {
-    title: 'hahaha',
-    msg: 'Hello',
-  },
-});
+console.log(1);
